2e15f99d3f8b4fb949a72d8688cbb6046d5bac96
kevin <kevin.aisellsit@outlook.com>
2025-05-16 10:01:31 -0500

    chore: finalize fixes for TS errors and API tests

--- functions/index.ts
+++ functions/index.ts
@@ -7,21 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import * as admin from "firebase-admin";
-import { initializeApp } from "firebase-admin/app";
-// import { configureGenkit } from "./src/genkitSetup"; // If using Genkit with Firebase plugin
+import * as admin from 'firebase-admin';
 
-// Initialize Firebase Admin SDK
-// It's recommended to initialize without arguments if deploying to Firebase,
-// as it will automatically use the project's service account.
-// If running locally or in other environments, you might need to provide credentials.
-if (admin.apps.length === 0) {
-  initializeApp();
+// Initialize the Admin SDK once
+if (!admin.apps.length) {
+  admin.initializeApp();
 }
-
-// Initialize Genkit (if applicable and using Firebase plugin)
-// configureGenkit();
-
 
 // Export functions from their respective files
 export * from "./src/userManagement";
